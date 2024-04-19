bd4ead6ac2db65e46a833ed6a6ba22911eef24d9
{Anwar <anwarelghazi8596@gmail.com>
2024-04-19 15:22:39 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const loadContents = document.getElementById('text');
+    loadContents.textContent ='This is really cool!'
+})
